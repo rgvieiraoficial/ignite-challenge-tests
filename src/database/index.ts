70d090f2e4d5c78f28d9e49fa51b916c0480afef
rgvieira <yami.hiei87@gmail.com>
2023-04-05 18:56:51 -0300

    Changed database port in database/index and docker-compose

--- src/database/index.ts
+++ src/database/index.ts
@@ -1,13 +1,13 @@
-import { createConnection, getConnectionOptions } from 'typeorm';
+import { Connection, createConnection, getConnectionOptions } from 'typeorm';
 
-interface IOptions {
-  host: string;
-}
+export default async (host = 'database'): Promise<Connection> => {
+  const defaultOptions = await getConnectionOptions();
 
-getConnectionOptions().then(options => {
-  const newOptions = options as IOptions;
-  newOptions.host = 'database'; // utilizando o nome do serviço
-  createConnection({
-    ...options,
-  }).then(() => console.log('Database connected'))
-});
+  return createConnection(
+    Object.assign(defaultOptions, {
+      host: process.env.NODE_ENV === 'test' ? 'localhost' : host, //Essa opção deverá ser EXATAMENTE o nome dado ao service do banco de dados
+      port: '5432',
+      database: process.env.NODE_ENV === 'test' ? 'rentx_test' : defaultOptions.database
+    })
+  );
+};
